5bca0242f40d44aa64c6ce121971f147415b8a47
Alex Ko <ko.e.alexander@gmail.com>
2020-08-09 14:38:18 -0400

    8/9/2020

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,2 +1,20 @@
 import 'package:flutter/material.dart';
+import '../widgets/auth_form.dart';
 
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        title: Text('asdf'),
+      ),
+      body: AuthForm(),
+    );
+  }
+}
---
+++ lib/widgets/auth_form.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: 'Email address'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Username'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 12),
+                  RaisedButton(
+                    child: Text('Login'),
+                    onPressed: () {},
+                  ),
+                  FlatButton(
+                    child: Text('Create new account'),
+                    textColor: Theme.of(context).primaryColor,
+                    onPressed: (){},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
