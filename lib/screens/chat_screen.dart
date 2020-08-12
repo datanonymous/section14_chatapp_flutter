73b6388b1731d714b592c1e3b94923aee19d797a
Alex Ko <ko.e.alexander@gmail.com>
2020-08-11 20:00:12 -0400

    8/11/2020

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import '../widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -28,34 +29,44 @@ class ChatScreen extends StatelessWidget {
                 value: 'logout',
               ),
             ],
-            onChanged: (itemIdentifier){
-              if(itemIdentifier == 'logout'){
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
                 FirebaseAuth.instance.signOut();
               }
             },
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('chats/Gbpt8A8N1eUEvpGyFnll/messages')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.documents;
-          return ListView.builder(
-            itemCount: streamSnapshot.data.documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8.0),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
             ),
-          );
-        },
+          ],
+        ),
       ),
+// build from earlier demonstrating messages
+//      StreamBuilder(
+//        stream: Firestore.instance
+//            .collection('chats/Gbpt8A8N1eUEvpGyFnll/messages')
+//            .snapshots(),
+//        builder: (ctx, streamSnapshot) {
+//          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+//            return Center(
+//              child: CircularProgressIndicator(),
+//            );
+//          }
+//          final documents = streamSnapshot.data.documents;
+//          return ListView.builder(
+//            itemCount: streamSnapshot.data.documents.length,
+//            itemBuilder: (ctx, index) => Container(
+//              padding: EdgeInsets.all(8.0),
+//              child: Text(documents[index]['text']),
+//            ),
+//          );
+//        },
+//      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
