3c4d2bcf76837675641e7d8ff8ecaec3c3fd8603
Alex Ko <ko.e.alexander@gmail.com>
2020-08-14 18:04:54 -0400

    8/14/2020

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -27,6 +27,7 @@ class Messages extends StatelessWidget {
                 itemCount: chatSnapshot.data.documents.length,
                 itemBuilder: (ctx, index) => MessageBubble(
                   chatDocs[index]['text'],
+                  chatDocs[index]['username'],
                   chatDocs[index]['userId'] == futureSnapshot.data.uid,
                   key: ValueKey(chatDocs[index].documentID),
                 ),
